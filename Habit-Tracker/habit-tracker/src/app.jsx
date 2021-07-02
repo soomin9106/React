301fea317e943c36e280f851e1b721875a792ce1
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-07-02 13:48:36 +0900

    React : copy directory

---
+++ Habit-Tracker/habit-tracker/src/app.jsx
@@ -0,0 +1,14 @@
+import './app.css';
+
+function App() {
+  return (
+    <ul>
+      <li>1. first</li>
+      <li>2. second</li>
+      <li>3. third</li>
+
+    </ul>
+  );
+}
+
+export default App;
