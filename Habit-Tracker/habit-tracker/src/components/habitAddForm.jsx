8357612bf84da5925eecfd75b7aa9c09717089e9
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-07-04 13:12:27 +0900

    React : function , memo

--- Habit-Tracker/habit-tracker/src/components/habitAddForm.jsx
+++ Habit-Tracker/habit-tracker/src/components/habitAddForm.jsx
@@ -1,23 +1,45 @@
-import React, { PureComponent } from 'react';
+import React, {memo} from 'react';
 
-class HabitAddForm extends PureComponent {
-    inputRef=React.createRef();
-    onSubmit= event =>{
-        event.preventDefault();
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        this.inputRef.current.value='';
-    }
-    render() {
-        console.log('habitAddForm');
+const habitAddForm = memo(
+    props => {
+        const inputRef=React.createRef();
+        const onSubmit = event =>{
+            event.preventDefault();
+            const name = inputRef.current.value;
+            name && props.onAdd(name);
+            inputRef.current.value='';
+        };
         return (
-            <form className="add-form" onSubmit={this.onSubmit}>
-                <input ref={this.inputRef} type="text" className="add-input" 
-                placeholder="Habit" />
-                <button className="add-button">Add</button>
-            </form>
+            <form className="add-form" onSubmit={onSubmit}>
+            <input ref={inputRef} type="text" className="add-input" 
+            placeholder="Habit" />
+            <button className="add-button">Add</button>
+            </form>       
         );
     }
-}
+);
 
-export default HabitAddForm;
+export default habitAddForm;
+
+//class 부분은 주석 처리 
+//class HabitAddForm extends PureComponent {
+//inputRef=React.createRef();
+//    onSubmit= event =>{
+//        event.preventDefault();
+//        const name = this.inputRef.current.value;
+//        name && this.props.onAdd(name);
+//        this.inputRef.current.value='';
+//    }
+//    render() {
+//        console.log('habitAddForm');
+//        return (
+//            <form className="add-form" onSubmit={this.onSubmit}>
+//                <input ref={this.inputRef} type="text" className="add-input" 
+//                placeholder="Habit" />
+//                <button className="add-button">Add</button>
+//           </form>
+//       );
+//        }
+//}
+
+//export default HabitAddForm;
