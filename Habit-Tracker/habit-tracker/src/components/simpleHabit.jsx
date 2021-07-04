b00c18f36417f7e7923fa3c1a1213cc16140943b
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-07-04 16:36:50 +0900

    React : React Hook 2

--- Habit-Tracker/habit-tracker/src/components/simpleHabit.jsx
+++ Habit-Tracker/habit-tracker/src/components/simpleHabit.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 //React Hook 
 const SimpleHabit = props => {
     const [count,setCount] = useState(0);
+    const spanRef = useRef(); 
 
-    const handleIncrement = () => {
+    const handleIncrement = useCallback(() => {
+        console.log('handling increment');
         setCount(count + 1);
-    };
+    });
+
+    const handleDecrement =useCallback(() => {
+        console.log('handling decrement');
+        setCount(count-1);
+    });
+
+    useEffect(() => {
+        console.log(`mounted and updated! ${count}`);
+    }, [count]);
 
     return (
         <li className="habit">
-            <span className="habit-name">Reading</span>
+            <span ref={spanRef} className="habit-name">Reading</span>
             <span className="habit-count">{count}</span>
             <button className="habit-button habit-increase" onClick={handleIncrement}>
             <i className="fas fa-plus-square"></i>
+            </button>
+            <button className="habit-button habit-decrease" onClick={handleDecrement}>
+            <i className="fas fa-minus-square"></i>
             </button>
         </li>
     );
